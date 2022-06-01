ccfc5b9a8eaa84b42a344f153223389360fc61ca
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-06-01 22:44:24 +0200

    done task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+
+startBtn.addEventListener('click', changeBackgroundColor);
+stopBtn.addEventListener('click', stopInterval);
+
+function changeBackgroundColor() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  stopBtn.removeAttribute('disabled');
+  startBtn.setAttribute('disabled', '');
+}
+
+function stopInterval() {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', '');
+}
